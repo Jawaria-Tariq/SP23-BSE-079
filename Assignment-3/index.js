464a04ed7ae5e07d7debc0bc52c6bf668f42fd98
Jawaria-Tariq <SP23-BSE-079@cuilahore.edu.pk>
2024-11-11 21:47:38 +0500

    Update index.js

--- Assignment-3/index.js
+++ Assignment-3/index.js
@@ -4,9 +4,7 @@ const app = express();
 app.set('views', __dirname + '/view-ejs');
 app.set("view engine", "ejs");
 
-
 app.use(express.static("public"));
-
 
 app.get("/", function(req, res) {
     res.render("bootstrap");
@@ -15,7 +13,6 @@ app.get("/", function(req, res) {
 app.get("/portfolio", function(req, res) {
     res.render("portfolio");
 });
-
 
 app.listen(7000, () => {
     console.log("Server started at localhost:7000");
